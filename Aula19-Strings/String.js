8e7a464cc3a691113cb11d5620a06c5512c1f1f0
Angelo Lima <angelolimadias@gmail.com>
2018-08-30 20:25:00 -0300

    aula19

---
+++ Aula19-Strings/String.js
@@ -0,0 +1,11 @@
+const escola = "angelo"
+
+console.log(escola.charAt(4)) 
+console.log(escola.charAt(6)) 
+console.log(escola.charCodeAt(3)) 
+console.log(escola.indexOf('3'))
+console.log(escola.substring(1)) 
+console.log(escola.substring(0,3)) 
+console.log("Escola ".concat(escola).concat("!"))
+console.log(escola.replace('a', 'e'))
+console.log("ana,maria,pedro".split(','))
