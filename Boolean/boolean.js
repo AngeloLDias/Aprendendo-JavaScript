9b1c815f1e0275c62f32a7bfda6cc8eaa5ea4df8
Angelo Lima <angelolimadias@gmail.com>
2018-08-30 21:56:53 -0300

    boolean

---
+++ Boolean/boolean.js
@@ -0,0 +1,31 @@
+var isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 2
+console.log(!!isAtivo)
+console.log("Verdadeiros...")
+console.log(!!3)
+console.log(!!-1)
+console.log(!!" ")
+console.log(!!"texto")
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo = true))
+
+console.log("Os Falsos...")
+console.log(!!0)
+console.log(!!"")
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+console.log("Pra finalizar...")
+console.log(!!("" || null || 0 || " "))
+
+var nome = "angelo"
+console.log(nome || "desconhecido")
