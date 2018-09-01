b49e560d72825bbb47f50f7a2be0b0f42804a998
Angelo Lima <angelolimadias@gmail.com>
2018-08-31 22:21:27 -0300

    Array

---
+++ Array/arrays.js
@@ -0,0 +1,15 @@
+var valores = [7.7,8.8,7,5,1]
+console.log(valores[0], valores[3])
+console.log(valores[5])
+
+valores[5] = 10
+console.log(valores)
+console.log(valores.length)
+
+valores.push({id: 3}, false, null, "teste")
+console.log(valores)
+
+console.log(valores.pop())
+delete valores[0]
+console.log(valores)
+console.log(typeof valores)
