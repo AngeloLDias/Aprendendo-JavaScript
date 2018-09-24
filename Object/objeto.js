584a2b5738108a949a20d5c0caf887b95df4ec82
Angelo Lima <angelolimadias@gmail.com>
2018-09-24 20:32:14 -0300

    object

--- Object/objeto.js
+++ Object/objeto.js
@@ -1,1 +1,17 @@
-var produto = {}
+const produto = {}
+produto.nome= "celular ultra mega";
+produto.preco= 4995.90
+produto['desconto legal'] = 0.40
+
+const produto2={
+    nome:"carro",
+    preco:200.00,
+    objeto:{
+        bala:"morango",
+        objeto:{
+            bala:"chocolete"
+        }
+    }
+}
+console.log(produto)
+console.log(produto2)
