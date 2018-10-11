6889fc7aba685bbd4b48da3194ce82266f7e7c15
Angelo Lima <angelolimadias@gmail.com>
2018-10-11 12:09:13 -0300

    melhorando codigo

--- JavaScript-challenges/Adivinhar/js/script.js
+++ JavaScript-challenges/Adivinhar/js/script.js
@@ -1,24 +1,24 @@
-function verificar(){
-    // var cont = document.querySelector('#contador').textContent;
-    var num1 = document.getElementById('n1').innerHTML;
-    var num2 = document.getElementById('n2').value;
-    if(num1 == num2){
-        alert('ACERTOU');
-        // contar()
-    }
-    else{
-        alert('ERROU');
-    }
-    reset();
-}
+var btnVerificar = document.querySelector('#btnVereficar');
+var resultado = document.querySelector('.resultado');
 
-function reset(){
+window.addEventListener('input', function () {
+    var num1 = document.querySelector('#n1').textContent;
+    var num2 = document.querySelector('#n2');
+    if (num1 == num2.value) {
+        resultado.classList.add('rTrue');
+        resultado.innerHTML = 'CERTO';
+        resultado.classList.remove('rFalse');
+        reset()
+    } else {
+        resultado.innerHTML = 'ERRADO';
+        resultado.classList.add('rFalse');
+
+    }
+
+})
+
+function reset() {
     document.getElementById('n2').value = "";
-    var r = Math.floor(Math.random() * 100);
-    document.getElementById('n1').innerHTML = r;
+    var num1 = Math.floor(Math.random() * 100);
+    document.getElementById('n1').innerHTML = num1;
 }
-
-// function contar(){
-//     document.getElementById('contador').value = +1;
-// };
-
