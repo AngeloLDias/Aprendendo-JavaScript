ec30ca2026790c4e1342f34d1ed36c71e8a27197
Breno Nunes <brenontavares@gmail.com>
2018-10-11 16:15:47 -0300

    Contador de Acertos

--- JavaScript-challenges/Adivinhar/js/script.js
+++ JavaScript-challenges/Adivinhar/js/script.js
@@ -8,7 +8,8 @@ window.addEventListener('input', function () {
         resultado.classList.add('rTrue');
         resultado.innerHTML = 'CERTO';
         resultado.classList.remove('rFalse');
-        reset()
+        reset();
+        somarContador();
     } else {
         resultado.innerHTML = 'ERRADO';
         resultado.classList.add('rFalse');
@@ -18,7 +19,24 @@ window.addEventListener('input', function () {
 })
 
 function reset() {
+    setTimeout( function (){
     document.getElementById('n2').value = "";
     var num1 = Math.floor(Math.random() * 100);
     document.getElementById('n1').innerHTML = num1;
+    }
+    , 800)
+}
+
+function somarContador(){
+    setTimeout( function(){
+        var correto = document.querySelector(".corretos");
+        var valorInteiro = parseInt(correto.textContent);
+        var soma = parseInt(valorInteiro) + parseInt(1);
+        correto.textContent = soma;
+    }, 450);
+    var correto = document.querySelector(".corretos");
+    correto.classList.add("animationContador");
+    setTimeout(function(){
+        correto.classList.remove("animationContador")
+    }, 440)
 }
